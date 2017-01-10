45ca27af38b33ec880a08607477849bdab616c8b
devidor <devidor@syneforge.com>
2017-01-10 10:56:00 +0200

    #0: Fix gitignore

---
+++ app/assets/tile.py
@@ -0,0 +1,23 @@
+import pytmx
+
+from ..collider import spatial_hash
+
+
+class TiledReader(object):
+
+    @classmethod
+    def read_and_add_collision(cls, fname):
+        tiled_map = pytmx.TiledMap(fname)
+        collision_layers = (layer for layer in tiled_map.layers
+                            if isinstance(layer, pytmx.TiledObjectGroup))
+        for layer in collision_layers:
+            for obj in layer:
+                box = cls._get_obj_box(obj)
+                spatial_hash.insert_object_for_box(box, obj)
+
+    @staticmethod
+    def _get_obj_box(obj):
+        return {
+            'min': (obj.x, obj.y),
+            'max': (obj.x + obj.width, obj.y + obj.height)
+        }
