78289b4aa5b4ecd3ad82694cc74daa3f6c8312b4
devidor <bogdankurinniy.dev1@gmail.com>
2017-01-22 15:10:39 +0200

    T0: Add first ability icon box

--- client/src/components/abilities.js
+++ client/src/components/abilities.js
@@ -2,52 +2,48 @@ define(['app', 'jquery', 'backbone', 'easel'], function(app, $, Backbone) {
 
     var Skill = function() {
         this.stage = app.stage;
+        this._lastX = -25;
         this.initCoords = {
+            x: this._lastX,
+            y: -100,
+            width: 50,
+            height: 50
+        };
+        this.container = new createjs.Container().set({
             x: app.canvas.width / 2,
             y: app.canvas.height
-        };
-        this.container = new createjs.Container().set(this.initCoords);
+        });
         this.stage.addChild(this.container);
     };
 
     Skill.prototype.add = function(skillInfo) {
-        var lastCoords, el;
-        if (this.container.children.length === 0) {
-            lastCoords = {
-                x: -50,
-                y: -100,
-                width: 50,
-                height: 50
-            };
-        } else {
-            for (var i = 0; i < this.container.children.length; i++) {
-                el = this.container.children[i];
-                el.x = -el.x - (this.container.children.length * 4 / el.width);
-                lastCoords = {
-                    x: el.x - el.width / 4,
-                    y: -100,
-                    width: el.width,
-                    height: el.height
-                };                
-            }
+        var childsCount = this.container.children.length;
+        if (childsCount > 0) {
+            this.initCoords.x = this._lastX - (childsCount * 3 * this.initCoords.width / 4);
+            console.log(this.initCoords.x)
         }
-        console.log(lastCoords);
-        var traitShape = new createjs.Shape();
-        var g = traitShape.graphics;
+        for (var i = 0; i < childsCount; i++) {
+            var shape = this.container.children[i];
+            this._drawRect(shape);
+            this.initCoords.x += 3 * this.initCoords.width / 2;
+        }
+        var shape = this._drawRect();
+        this.container.addChild(shape);
+    };
+
+    Skill.prototype._drawRect = function(shape) {
+        var shape = shape || new createjs.Shape();
+        var g = shape.graphics;
+        g.clear();
         g.setStrokeStyle(0.5);
         g.beginStroke("#A9A9A9");
         g.drawRoundRect(
-            lastCoords.x,
-            lastCoords.y,
-            lastCoords.width,
-            lastCoords.height,
+            this.initCoords.x,
+            this.initCoords.y,
+            this.initCoords.width,
+            this.initCoords.height,
             3);
-        traitShape.width = lastCoords.width;
-        traitShape.height = lastCoords.height;
-
-        this.container.addChild(traitShape);
-
-        window.qwe = traitShape;
+        return shape;
     };
 
     Skill.prototype.remove = function(shape) {
@@ -61,6 +57,7 @@ define(['app', 'jquery', 'backbone', 'easel'], function(app, $, Backbone) {
     Skill.create = function(character) {
         var skill = new Skill();
         // TODO: Add skills in future;
+        skill.add();
         skill.add();
         // skill.add();
         skill.add();
